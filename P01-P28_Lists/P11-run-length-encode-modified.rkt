e089c53fc1bd8be37ac46761b810bbe41bfe0e8f
Chris Vig <chris@invictus.so>
2016-07-17 10:51:08 -0500

    Implement P11.

---
+++ P01-P28_Lists/P11-run-length-encode-modified.rkt
@@ -0,0 +1,48 @@
+#!/usr/bin/racket
+#lang racket
+
+; P11-run-length-encode-modified.rkt
+; Chris Vig (chris@invictus.so)
+
+; -- Requires --
+
+(require "P04-count-list.rkt")
+(require "P09-pack-dups.rkt")
+
+; -- Provides --
+
+(provide run-length-encode-modified)
+
+; -- Public Procedures --
+
+; Returns a modified run-length encoding representation of a list.
+; e.g., (run-length-encode-modified '(a a a b b c d d d)) -> '((3 a) (2 b) c (3 d))
+(define/contract (run-length-encode-modified lst)
+  (-> list? list?)
+  (let iterate ([lst (pack-dups lst)])
+    (if (null? lst)
+        null
+        (let* ([sublist (car lst)]
+               [item (car sublist)]
+               [count (count-list sublist)]
+               [value-to-append (if (= 1 count)
+                                    item
+                                    (list count item))])
+          (cons value-to-append (iterate (cdr lst)))))))
+
+; -- Unit Tests --
+
+(module+ test
+  (require rackunit)
+
+  (test-case "run-length-encode-modified"
+    (define (test input expected)
+      (check-equal? (run-length-encode-modified input) expected))
+    (test null null)
+    (test '(a) '(a))
+    (test '(a b c) '(a b c))
+    (test '(a a a b b c) '((3 a) (2 b) c))
+    (test '(a b b c c c) '(a (2 b) (3 c))))
+
+  (test-case "run-length-encode-modified contract"
+    (check-exn exn:fail:contract? (λ () (run-length-encode-modified 0+1i)))))
