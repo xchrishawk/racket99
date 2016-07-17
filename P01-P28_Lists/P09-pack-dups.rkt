9e53d8cde3fbf1b9df776f130124b6a6485c7e1b
Chris Vig <chris@invictus.so>
2016-07-16 20:33:25 -0500

    Implement P09.

---
+++ P01-P28_Lists/P09-pack-dups.rkt
@@ -0,0 +1,52 @@
+#!/usr/bin/racket
+#lang racket
+
+; P09-pack-dups.rkt
+; Chris Vig (chris@invictus.so)
+
+; -- Requires --
+
+(require "P05-reverse-list.rkt")
+
+; -- Provides --
+
+(provide pack-dups)
+
+; -- Public Procedures --
+
+; Packs duplicate entries in a list into sublists.
+; e.g., (pack-dups '(a a a b b c d d)) -> '((a a a) (b b) (c) (d d))
+(define/contract (pack-dups lst)
+  (-> list? list?)
+
+  (define (iterate in out working)
+    (cond
+
+      ; No input left - terminating condition
+      [(null? in)
+       (cons working out)]
+
+      ; Either the working set is empty (first iteration), or the next value is equal to working value
+      [(or (null? working) (equal? (car in) (car working)))
+       (iterate (cdr in) out (cons (car in) working))]
+
+      ; Not first iteration, and next value is not equal to working value
+      [else
+       (iterate (cdr in) (cons working out) (list (car in)))]))
+
+  (if (null? lst)
+      null
+      (reverse-list (iterate lst null null))))
+
+; -- Unit Tests --
+
+(module+ test
+  (require rackunit)
+  (test-case "pack-dups"
+    (check-equal? (pack-dups null) null)
+    (check-equal? (pack-dups '(1)) '((1)))
+    (check-equal? (pack-dups '(1 1 1)) '((1 1 1)))
+    (check-equal? (pack-dups '(1 2 2 3 3 3)) '((1) (2 2) (3 3 3)))
+    (check-equal? (pack-dups '(1 1 1 2 2 3)) '((1 1 1) (2 2) (3))))
+  (test-case "pack-dups contract"
+    (check-exn exn:fail:contract? (λ () (pack-dups 14)))))
